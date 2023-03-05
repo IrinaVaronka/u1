67cb9afe8829a1733929534c2590ccc9a9d88a27
IrinaVaronka <varonkairina@gmail.com>
2023-03-05 15:12:37 +0200

    write name

--- src/Components/accountList.jsx
+++ src/Components/accountList.jsx
@@ -1,42 +1,13 @@
 
+
 function AccountList () {
 
-const name1 = [
-    {name: 'Maria', lastname: 'Gaga', amount: 16},
-    {name: 'Filip', lastname: 'Li', amount: 59}
-]
 
 
 
-    return (
 
-        <table className="table table-dark table-striped table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">Account amount, $ </th>
-      <th scope="col">Delete account</th>
-      <th scope="col">Enter value</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-        {
-            name1.map((n, i) => <th key={i} scope="row">{n.name}</th>)
-        }
-      <td>Jackson</td>
-      <td>99</td>
-      <td> <button className="btn btn-primary" type="submit">Delete</button></td>
-      <td> 
-        <input className="text form-control"></input>
-        <button className="btn btn-primary add" type="submit">Add funds</button>
-        <button className="btn btn-primary deduct" type="submit">Deduct funds</button></td>
-      <td></td>
-    </tr>
-  </tbody>
-</table>
+    return 
 
-    )
+    
 }
 export default AccountList;
--- src/Components/newAccount.jsx
+++ src/Components/newAccount.jsx
@@ -1,19 +1,70 @@
+import { useState } from "react";
 
 function NewAccount () {
 
+const [name, setName] = useState('');
+const [lastName, setLastName] = useState('');
+const [fullList, setFullList] = useState([]);
 
+
+const doName = e => {
+    setName(e.target.value);
+}
+const doLastName = e => {
+    setLastName(e.target.value);
+}
+
+const addName = () => {
+    setFullList(n => [...n, {name, lastName}]);
+}
 
 
 
     return (
-
+<>
         <div className="input-group col-sm-7">
         <span className="input-group-text">First name</span>
-        <input className="text" aria-label="First name" class="form-control"></input>
+        <input className="text" aria-label="First name" class="form-control"onChange={doName} value={name}/>
         <span className="input-group-text">Last name</span>
-        <input className="text" aria-label="Last name" class="form-control"></input>
-        <button className="btn btn-primary" type="submit">Create new account</button>
+        <input className="text" aria-label="Last name" class="form-control" onChange={doLastName} value={lastName}/>
+        <button className="btn btn-primary" type="create" onClick={addName}>Create new account</button>  
         </div>
+
+<div className="container">
+<div className="row align-items-start">
+<ul className="col">
+<li className="list-group-item">Name</li>
+</ul>
+<ul className="col">
+<li className="list-group-item">Last Name</li>
+</ul>
+<ul className="col">
+<li className="list-group-item">Account amount</li>
+</ul>
+
+</div>
+</div>  
+
+<div className="container">
+<div className="row align-items-start">
+  <ul className="col">
+    {
+      
+      fullList.map((n, i) => <li key={i} className="list-group-item">{n.name}</li>)
+    }
+  </ul>
+  <ul className="col">
+    {
+    fullList.map((n, i) => <li key={i} className="list-group-item">{n.lastName}</li>)
+    }
+  </ul>
+  <ul className="col">
+    
+  </ul>
+</div>
+</div>
+   
+</>
 
     )
 }
